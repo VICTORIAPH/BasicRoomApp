d1b85cc8c4a8f26724479cfe13e8f63f581f1198
victo <vicky454ph@gmail.com>
2022-04-28 15:21:33 -0500

    agregamos la clase MyApplication

---
+++ app/src/main/java/com/example/basicroomapp/database/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.basicroomapp.database
+
+import android.app.Application
+
+class MyApplication: Application() {
+    override fun onCreate(){
+        DatabaseManager.instance.initializeDb(applicationContext)
+        super.onCreate()
+    }
+
+}
